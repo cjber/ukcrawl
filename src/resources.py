0207b6f9411be8bed0576c6e5873fcda050e852c
cjber <cjberragan@gmail.com>
2024-04-25 10:52:13 +0100

    limit number of cpu to 8

--- src/resources.py
+++ src/resources.py
@@ -9,8 +9,7 @@ class DaskResource(ConfigurableResource):
     n_workers: int
 
     def make_dask_cluster(self) -> Client:
-        client = Client(LocalCluster(n_workers=self.n_workers))
-        return client
+        return Client(LocalCluster(n_workers=self.n_workers))
 
 
 s3_resource = S3Resource(
@@ -18,4 +17,4 @@ s3_resource = S3Resource(
     aws_access_key_id=EnvVar("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=EnvVar("AWS_SECRET_ACCESS_KEY"),
 )
-dask_resource = DaskResource(n_workers=os.cpu_count() or 1)
+dask_resource = DaskResource(n_workers=min(os.cpu_count() or 1, 8))
